3fa9d91c7efebc9a3fb6b22d65f006eda7c9c688
徐博伟 <xubowei@xuboweideMacBook-Air.local>
2016-03-08 16:56:37 +0800

    1.29

---
+++ 1.29.rkt
@@ -0,0 +1,31 @@
+#lang racket
+(define (simpson f a b n)
+  (define (sum term a next b)
+    (if (> a b)
+        0
+        (+ (term a)
+           (sum term (next a) next b))))
+  
+  (define h (/ (- b a)
+               n))
+  (define (y k)
+    (f (+ a (* k h))))
+  (define (factor k)
+    (cond ((or (= k 1) (= k 0))
+           1)
+          ((odd? k)
+           4)
+          (else
+           2)))
+  (define (term k)
+    (* (factor k) (y k)))
+
+  (define (next k)
+    (+ k 1))
+
+  (if (not (even? n))
+        (error "n can't be odd")
+        (* (/ h 3)
+           (sum term 0.0 next n))))
+(define (cube x) (* x x x))
+(simpson cube 0 1 1000)
